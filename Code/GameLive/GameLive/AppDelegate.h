5f13dcf498db8a24aba0209a99632eea0c9cc50e
minmin <1838115981@qq.com>
2016-06-26 14:57:39 +0800

    游戏直播项目搭建
    测试上传

---
+++ Code/GameLive/GameLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameLive
+//
+//  Created by minmin on 16/6/26.
+//  Copyright © 2016年 minmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Code/GameLive/GameLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameLive
+//
+//  Created by minmin on 16/6/26.
+//  Copyright © 2016年 minmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
